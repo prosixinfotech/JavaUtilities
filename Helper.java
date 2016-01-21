af86f705b0239e1a9d8406547f8d612d9662f7c9
Arjun Prasad <arjunprasad.javax@gmail.com>
2016-01-21 12:25:55 +0530

    Added Helper.java by Arjun after making a clone of original project on github

---
+++ Helper.java
@@ -0,0 +1,40 @@
+package com.xuggle.util;
+
+import java.awt.Component;
+import java.util.Properties;
+import javax.swing.JOptionPane;
+
+public class Helper {
+    
+    private static final String dialogTitle="OpenCV";
+    
+    
+    public static class MessageHelper{
+        public static void showInfoDialog(Component component,String message,String title){
+            JOptionPane.showMessageDialog(component, message, (title==null ? dialogTitle : title), JOptionPane.INFORMATION_MESSAGE);
+        }
+        
+        public static void showErrorDialog(Component component,String message,String title){
+            JOptionPane.showMessageDialog(component, message, (title==null ? dialogTitle : title), JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    public static class PropertiesHelper{        
+        
+        public static Properties getProperties(String file) throws Exception{            
+            Properties properties=new Properties();
+//            properties.load(new FileInputStream(file));
+            properties.load(ClassLoader.getSystemResourceAsStream(file));
+            return properties;
+        }
+    }      
+    
+    public static void closeCloseable(java.io.Closeable closeable){
+        try {
+            closeable.close();
+        } catch (Exception e) {
+            AppLooger.logError(Helper.class, e.getMessage());
+        }
+    }
+    
+}
